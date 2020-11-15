0b8011a3d8c1f28eeb6c2897c14a794c5907cb07
Erik McLaughlin <erik@erikcmclaughlin.com>
2020-11-15 13:28:43 -0600

    Fixed get_member bug

--- main.py
+++ main.py
@@ -6,35 +6,34 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD_ID = int(os.getenv('GUILD_ID'))
-USER_ID = os.getenv('USER_ID')
+USER_ID = int(os.getenv('USER_ID'))
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
 if TOKEN is None or USER_ID is None or CHANNEL_ID is None:
     print("Failed to load env variables!")
     exit(0)
 
-client = discord.Client()
-guild = client.get_guild(GUILD_ID)
-print(guild)
-print(guild.id)
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
 
 
 async def move_user_to_channel():
+    guild = client.get_guild(GUILD_ID)
+
     channel = client.get_channel(CHANNEL_ID)
     # channel now holds the channel you want to move people into
     if channel is None:
         print("Cannot move user: Invalid Channel ID")
         return None
 
-    member = guild.get_member(USER_ID)    #commands.converter.MemberConverter().convert(guild, USER_ID)
-    #member = client.get_user(767550258820677642)   #USER_ID)
+    member = guild.get_member(USER_ID)
     # member now holds the user that you want to move
     if member is None:
-        print("Cannot move user: Invalid User ID")
+        print("Cannot move user: Invalid User ID " + repr(USER_ID))
         return None
 
-    #await member.
     await member.edit(voice_channel=channel)
+    print("Moved user to channel " + repr(channel.id))
     #await channel.connect()
 
 
